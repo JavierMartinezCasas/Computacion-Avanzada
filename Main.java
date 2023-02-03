2cb6af618c135911379828b11e48a9c78aa9c776
Javier <100429142@alumnos.uc3m.es>
2023-02-03 18:21:54 +0100

    Tiempo de Sergio

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        var inicio = System.currentTimeMillis();
         int num = 29;
         boolean isPrime = true;
 
@@ -14,5 +15,8 @@ public class Main {
             System.out.println(num + " es un número primo");
         else
             System.out.println(num + " no es un número primo");
+        var fin = System.currentTimeMillis();
+        var result = fin - inicio;
+        System.out.println("El tiempo ha sido: " + result + "ms");
     }
 }
