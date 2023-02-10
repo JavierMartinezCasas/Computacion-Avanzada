d150459f84cf9067ad8f3aeabd92aa34c56b27b9
sergio <100429119@alumnos.uc3m.es>
2023-02-10 18:25:05 +0100

    cosas

--- Parte2/Main.java
+++ Parte2/Main.java
@@ -4,75 +4,88 @@ import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) {
-        var start = System.currentTimeMillis();
+        ArrayList<Integer> input1 = new ArrayList<Integer>();
+        ArrayList<Integer> input2 = new ArrayList<Integer>();
 
-        int num = 90;
-        int num_max = num;
+        ArrayList<Integer> output = new ArrayList<Integer>();
+        ArrayList<Long> times = new ArrayList<Long>();
+        int num = 21;
+        int num2 = 34;
+        for (int j=0; j<4; j++) {
+            var start = System.nanoTime();
+            int num_max = num;
+            int num_max2 = num2;
 
-        int num2 = 900;
-        int num_max2 = num2;
 
-        ArrayList<Integer> facts1 = new ArrayList<Integer>();
-        ArrayList<Integer> facts2 = new ArrayList<Integer>();
+            ArrayList<Integer> facts1 = new ArrayList<Integer>();
+            ArrayList<Integer> facts2 = new ArrayList<Integer>();
 
-        // BUCLE ARRAY 1
-        var div = 2;
-        var stop = true;
-        while (stop) {
-            if (num_max / 2 <= div) {    // Se ha llegado a la mitad del numero entrada
-                stop = false;
-            } else if (num % div == 0) {
-                facts1.add(div);
-                num = num / div;
-            } else if (num % div != 0) {
-                div++;
+            input1.add(num);
+            input2.add(num2);
+
+            // BUCLE ARRAY 1
+            var div = 2;
+            var stop = true;
+            while (stop) {
+                if (num_max / 2 <= div) {    // Se ha llegado a la mitad del numero entrada
+                    stop = false;
+                } else if (num % div == 0) {
+                    facts1.add(div);
+                    num = num / div;
+                } else if (num % div != 0) {
+                    div++;
+                }
             }
-        }
 
-        // BUCLE ARRAY 2
-        div = 2;
-        stop = true;
-        while (stop) {
-            if (num_max2 / 2 <= div) {    // Se ha llegado a la mitad del numero entrada
-                stop = false;
-            } else if (num2 % div == 0) {
-                facts2.add(div);
-                num2 = num2 / div;
-            } else if (num2 % div != 0) {
-                div++;
+            // BUCLE ARRAY 2
+            div = 2;
+            stop = true;
+            while (stop) {
+                if (num_max2 / 2 <= div) {    // Se ha llegado a la mitad del numero entrada
+                    stop = false;
+                } else if (num2 % div == 0) {
+                    facts2.add(div);
+                    num2 = num2 / div;
+                } else if (num2 % div != 0) {
+                    div++;
+                }
             }
+
+            ArrayList<Integer> common = new ArrayList<Integer>();
+
+
+            stop = true;
+            var i = 0;
+            while (stop) {
+                var x = facts1.get(i);
+                if (facts2.contains(x)) {
+                    common.add(x);
+                    facts1.remove(i);
+                    facts2.remove((Integer) x);
+                } else {
+                    i++;
+                }
+                if (i == facts1.size()) {
+                    stop = false;
+                }
+            }
+
+            var numResult = 1;
+            for (i = 0; i < common.size(); i++) {
+                numResult *= common.get(i);
+            }
+            output.add(numResult);
+
+
+            int sum = num + num2;
+            num = num2;
+            num2 = sum;
+
+            var end = System.nanoTime();
+            var result = end - start;
+            times.add(result);
+
         }
-
-        System.out.println("La lista de divisores del primer numero es: " + facts1);
-        System.out.println("La lista de divisores del segundo  numero es: " + facts2);
-
-        ArrayList<Integer> common = new ArrayList<Integer>();
-        stop = true;
-        var i = 0;
-        while (stop){
-             var x = facts1.get(i);
-             if (facts2.contains(x)) {
-                 common.add(x);
-                 facts1.remove(i);
-                 facts2.remove((Integer) x);
-             } else {
-                 i++;
-             }
-             if (i==facts1.size()) {
-                 stop = false;
-             }
-        }
-
-        var numResult = 1;
-        for (i=0; i<common.size(); i++) {
-            numResult *= common.get(i);
-        }
-
-        var end = System.currentTimeMillis();
-        var result = end - start;
-
-        System.out.println("La lista resultado es: " + common);
-        System.out.println("El mcd es: " + numResult);
-        System.out.println("El tiempo ha sido: " + result + "ms");
+        System.out.println("El tiempo ha sido: " + times + "ns de: " + input1 + " " + input2);
     }
 }
