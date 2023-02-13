3419fbdbba55fd74520b8e2cb7d303a7bbbbf640
sergio <100429119@alumnos.uc3m.es>
2023-02-13 19:54:40 +0100

    cositas jevis

--- Parte2/Main_euclides.java
+++ Parte2/Main_euclides.java
@@ -1,24 +1,41 @@
 package Parte2;
 
+import java.util.ArrayList;
+
 public class Main_euclides {
     public static void main(String[] args) {
+        ArrayList<Integer> input1 = new ArrayList<Integer>();
+        ArrayList<Integer> input2 = new ArrayList<Integer>();
 
-        var start = System.currentTimeMillis();
+        ArrayList<Integer> output = new ArrayList<Integer>();
+        ArrayList<Long> times = new ArrayList<Long>();
+        var a = 377;
+        var b = 610;
 
-        var a = 7;
-        var b = 3;
+        for (int j=0; j<32; j++) {
+            var start = System.nanoTime();
+            input1.add(a);
+            input2.add(b);
+            int num_max = a;
+            int num_max2 = b;
 
-        while (b != 0) {  //  C + n(C + S) -> 1 + n(1 + 4)
-            int tmp = b;
-            b = a % b;
-            a = tmp;
+            while (b != 0) {  //  C + n(C + S) -> 1 + n(1 + 4)
+                int tmp = b;
+                b = a % b;
+                a = tmp;
+            }
+
+            int sum = num_max + num_max2;
+            a = num_max2;
+            b = sum;
+
+            // tiempo
+            var end = System.nanoTime();
+            long result = end - start;
+            times.add(result);
         }
-
-        // tiempo
-        var end = System.currentTimeMillis();
-        long result = end - start;
-
-        System.out.println("El factor cumún es: " + a); // 1
-        System.out.println("El tiempo ha sido: " + result + "ms"); // 1
+        System.out.println("El tiempo ha sido: \n" + times + "ns de: \n" + input1 + " \n" + input2);
+        //System.out.println("El factor cumún es: " + a); // 1
+        //System.out.println("El tiempo ha sido: " + result + "ms"); // 1
     }
 }
